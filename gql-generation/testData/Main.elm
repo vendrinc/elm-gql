35077dd4b09b3082b815ea53524fe4e522c8f113
Aaron White <aaron@singularity.vc>
2021-05-01 15:49:40 -0400

    Commit some WIP

--- gql-generation/testData/Main.elm
+++ gql-generation/testData/Main.elm
@@ -1,56 +1,9 @@
 module Main exposing (..)
 
-import Json.Decode as Decode exposing (Decoder)
-
-type AppAccessSource
-    = GSuite
-    | GoogleSAML
-    | Okta
-    | OneLogin
-    | Manual
-    | License
-    | Integration
-    | Microsoft
-    | JumpCloud
-
-
-list : List AppAccessSource
-list =
-    [ GSuite, GoogleSAML, Okta, OneLogin, Manual, License, Integration, Microsoft, JumpCloud ]
-
-decoder : Decoder AppAccessSource
-decoder =
-    Decode.string
-        |> Decode.andThen
-            (\string ->
-                case string of
-                    "GSuite" ->
-                        Decode.succeed GSuite
-
-                    "GoogleSAML" ->
-                        Decode.succeed GoogleSAML
-
-                    "Okta" ->
-                        Decode.succeed Okta
-
-                    "OneLogin" ->
-                        Decode.succeed OneLogin
-
-                    "Manual" ->
-                        Decode.succeed Manual
-
-                    "License" ->
-                        Decode.succeed License
-
-                    "Integration" ->
-                        Decode.succeed Integration
-
-                    "Microsoft" ->
-                        Decode.succeed Microsoft
-
-                    "JumpCloud" ->
-                        Decode.succeed JumpCloud
-
-                    _ ->
-                        Decode.fail ("Invalid AppAccessSource type, " ++ string ++ " try re-running the @dillonkearns/elm-graphql CLI ")
-            )
+query :
+    { -- Cool
+      app : { id : GQL.ID } -> GQL.App value -> GQL.Query (Maybe value)
+    }
+query =
+    { app = \_ _ -> Debug.todo "hehehe"
+    }
