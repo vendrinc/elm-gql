aaebcc7962b3b86cd234d51bd7fbccced970d56f
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2021-05-02 13:31:56 -0500

    invert nullability because elm

--- gql-generation2/src/GraphQL/Schema/Type.elm
+++ gql-generation2/src/GraphQL/Schema/Type.elm
@@ -2,7 +2,6 @@ module GraphQL.Schema.Type exposing
     ( Type(..)
     , decoder
     , toKind
-    , toString
     )
 
 import GraphQL.Schema.Kind as Kind exposing (Kind)
@@ -17,49 +16,66 @@ type Type
     | Union String
     | Interface String
     | List_ Type
-    | Non_Null Type
+    | Nullable Type
 
 
 decoder : Json.Decoder Type
 decoder =
+    innerDecoder
+        |> Json.map invert
+
+
+type Inner_Type
+    = Inner_Scalar String
+    | Inner_InputObject String
+    | Inner_Object String
+    | Inner_Enum String
+    | Inner_Union String
+    | Inner_Interface String
+    | Inner_List_ Inner_Type
+    | Inner_Non_Null Inner_Type
+
+
+innerDecoder : Json.Decoder Inner_Type
+innerDecoder =
     Json.field "kind" Json.string
         |> Json.andThen fromKind
 
 
-fromKind : String -> Json.Decoder Type
+fromKind : String -> Json.Decoder Inner_Type
 fromKind kind =
     case kind of
         "SCALAR" ->
-            Json.map Scalar nameDecoder
+            Json.map Inner_Scalar nameDecoder
 
         "INPUT_OBJECT" ->
-            Json.map InputObject nameDecoder
+            Json.map Inner_InputObject nameDecoder
 
         "OBJECT" ->
-            Json.map Object nameDecoder
+            Json.map Inner_Object nameDecoder
 
         "ENUM" ->
-            Json.map Enum nameDecoder
+            Json.map Inner_Enum nameDecoder
 
         "UNION" ->
-            Json.map Union nameDecoder
+            Json.map Inner_Union nameDecoder
 
         "INTERFACE" ->
-            Json.map Interface nameDecoder
+            Json.map Inner_Interface nameDecoder
 
         "LIST" ->
-            Json.map List_ (Json.field "ofType" lazyDecoder)
+            Json.map Inner_List_ (Json.field "ofType" lazyDecoder)
 
         "NON_NULL" ->
-            Json.map Non_Null (Json.field "ofType" lazyDecoder)
+            Json.map Inner_Non_Null (Json.field "ofType" lazyDecoder)
 
         _ ->
             Json.fail ("Unrecognized kind: " ++ kind)
 
 
-lazyDecoder : Json.Decoder Type
+lazyDecoder : Json.Decoder Inner_Type
 lazyDecoder =
-    Json.lazy (\_ -> decoder)
+    Json.lazy (\_ -> innerDecoder)
 
 
 nameDecoder : Json.Decoder String
@@ -67,61 +83,78 @@ nameDecoder =
     Json.field "name" Json.string
 
 
-toString : Type -> String
-toString type_ =
-    case type_ of
-        Scalar name ->
-            name
-
-        InputObject name ->
-            name
-
-        Object name ->
-            name
-
-        Enum name ->
-            name
-
-        Union name ->
-            name
-
-        Interface name ->
-            name
-
-        List_ child ->
-            "[" ++ toString child ++ "]"
-
-        Non_Null child ->
-            toString child ++ "!"
-
-
 
 -- Getting Kind
 
 
-toKind : Type -> Kind
+toKind : Inner_Type -> Kind
 toKind type_ =
     case type_ of
-        Scalar name ->
+        Inner_Scalar name ->
             Kind.Scalar name
 
-        InputObject name ->
+        Inner_InputObject name ->
             Kind.InputObject name
 
-        Object name ->
+        Inner_Object name ->
             Kind.Object name
 
-        Enum name ->
+        Inner_Enum name ->
             Kind.Enum name
 
-        Union name ->
+        Inner_Union name ->
             Kind.Union name
 
-        Interface name ->
+        Inner_Interface name ->
             Kind.Interface name
 
-        List_ child ->
+        Inner_List_ child ->
             toKind child
 
-        Non_Null child ->
+        Inner_Non_Null child ->
             toKind child
+
+
+
+-- INVERTING NULLABLE TRASH
+
+
+invert : Inner_Type -> Type
+invert =
+    invert_ True
+
+
+invert_ : Bool -> Inner_Type -> Type
+invert_ wrappedInNull inner =
+    let
+        nullable type_ =
+            if wrappedInNull then
+                Nullable type_
+
+            else
+                type_
+    in
+    case inner of
+        Inner_Non_Null inner_ ->
+            invert_ False inner_
+
+        Inner_List_ inner_ ->
+            nullable (List_ (invert_ True inner_))
+
+        Inner_Scalar value ->
+            nullable (Scalar value)
+
+        Inner_InputObject value ->
+            nullable (InputObject value)
+
+        Inner_Object value ->
+            nullable (Object value)
+
+        Inner_Enum value ->
+            nullable (Enum value)
+
+        Inner_Union value ->
+            nullable (Union value)
+
+        Inner_Interface value ->
+            nullable (Interface value)
