7e33a43ce9670b6bc486fa72e825df8c818dc6c0
Matthew Griffith <mdg.griffith@gmail.com>
2021-05-19 23:43:08 -0400

    Update Engine.elm

---
+++ gql-generation3/src/Elm/Gen/GraphQL/Engine.elm
@@ -0,0 +1,88 @@
+module Elm.Gen.GraphQL.Engine exposing (..)
+
+{-|
+This was generated from the docs of `GraphQL.Engine`
+
+-}
+
+import Elm
+
+
+arg =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "arg"
+
+
+decodeId =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "decodeId"
+
+
+encodeId =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "encodeId"
+
+
+enum =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "enum"
+
+
+field =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "field"
+
+
+fieldWith =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "fieldWith"
+
+
+map =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "map"
+
+
+map2 =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "map2"
+
+
+maybeEnum =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "maybeEnum"
+
+
+maybeScalarEncode =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "maybeScalarEncode"
+
+
+mutation =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "mutation"
+
+
+nullable =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "nullable"
+
+
+object =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "object"
+
+
+objectWith =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "objectWith"
+
+
+query =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "query"
+
+
+queryString =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "queryString"
+
+
+recover =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "recover"
+
+
+select =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "select"
+
+
+union =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "union"
+
+
+with =
+    Elm.valueFrom (Elm.moduleName [ "GraphQL", "Engine" ]) "with"
