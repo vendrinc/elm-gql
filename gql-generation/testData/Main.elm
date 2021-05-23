030d72e3a2c85587ea7e62da27689cfcbf89c90f
Aaron White <aaron@singularity.vc>
2021-05-23 13:35:09 -0400

    Remove defunct json based AST construction

--- gql-generation/testData/Main.elm
+++
@@ -1,9 +0,0 @@
-module Main exposing (..)
-
-query :
-    { -- Cool
-      app : { id : GQL.ID } -> GQL.App value -> GQL.Query (Maybe value)
-    }
-query =
-    { app = \_ _ -> Debug.todo "hehehe"
-    }
