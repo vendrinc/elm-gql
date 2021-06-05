4ff982204174b1ed10229b75f139164cdd690e5b
Matthew Griffith <mdg.griffith@gmail.com>
2021-06-05 16:03:53 -0400

    get rid of generation2 and move gen3 to top level

--- gql-generation2/src/Codegen/ElmCodegenUtil.elm
+++
@@ -1,8 +0,0 @@
-module Codegen.ElmCodegenUtil exposing (..)
-
-import Elm.CodeGen as Elm
-
-
-mergeLinkage : Elm.Linkage -> Elm.Linkage -> Elm.Linkage
-mergeLinkage l1 l2 =
-    Elm.combineLinkage [ l1, l2 ]
