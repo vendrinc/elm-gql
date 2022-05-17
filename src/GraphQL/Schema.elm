c0d3e7d6e043ecfa0087f90cd83282c5cf3c7ca8
Matthew Griffith <mdg.griffith@gmail.com>
2022-05-17 09:35:59 -0400

    expose interface details from schema

--- src/GraphQL/Schema.elm
+++ src/GraphQL/Schema.elm
@@ -4,7 +4,7 @@ module GraphQL.Schema exposing
     , Kind(..), Schema, Type(..)
     , mockScalar
     , Wrapped(..), getWrap, getInner
-    , Argument, Field, InputObjectDetails, Namespace, ObjectDetails, UnionDetails, Variant, kindToString, typeToElmString, typeToString
+    , Argument, Field, InputObjectDetails, InterfaceDetails, Namespace, ObjectDetails, UnionDetails, Variant, kindToString, typeToElmString, typeToString
     )
 
 {-|
