49d43d8e3ac57cef941d8cd4394b5247f85e68ba
Matthew Griffith <mdg.griffith@gmail.com>
2023-05-16 21:57:40 -0400

    Version 0.6.2

--- cli/bin.ts
+++ cli/bin.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 
 import * as Run from "./run";
 
-const version: string = "0.6.1";
+const version: string = "0.6.2";
 
 function collect(val: string, memo: string[]) {
   memo.push(val);
