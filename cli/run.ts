f71b83900092cc3db406a55fa53d249b28c7cf11
Matthew Griffith <mdg.griffith@gmail.com>
2023-06-27 18:24:08 +0200

    Bump to 0.6.5 - fix to mock generation

--- cli/run.ts
+++ cli/run.ts
@@ -10,7 +10,7 @@ import engine from "./templates/Engine.elm";
 // @ts-ignore
 globalThis["XMLHttpRequest"] = XMLHttpRequest.XMLHttpRequest;
 
-export const version: string = "0.6.4";
+export const version: string = "0.6.5";
 
 type Cache = {
   engineVersion: string;
