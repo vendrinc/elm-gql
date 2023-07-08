60c37271feee961ce46522aba798d1b2165d71cd
Matthew Griffith <mdg.griffith@gmail.com>
2023-07-08 10:49:36 -0400

    bump to 0.7.1 - fragment import fixes

--- cli/run.ts
+++ cli/run.ts
@@ -12,7 +12,7 @@ import decode from "./templates/Decode.elm";
 // @ts-ignore
 globalThis["XMLHttpRequest"] = XMLHttpRequest.XMLHttpRequest;
 
-export const version: string = "0.7.0";
+export const version: string = "0.7.1";
 
 type Cache = {
   engineVersion: string;
