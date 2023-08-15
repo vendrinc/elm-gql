9a4cf66b426e19835826c120a112ae6b846caf04
Matthew Griffith <mdg.griffith@gmail.com>
2023-08-15 19:15:35 -0400

    Add GraphQL.InputObject.elm

---
+++ src/GraphQL/InputObject.elm
@@ -0,0 +1,137 @@
+module GraphQL.InputObject exposing
+    ( InputObject, inputObject
+    , addField, addOptionalField
+    , maybeScalarEncode
+    , encode, toFieldList
+    )
+
+{-|
+
+@docs InputObject, inputObject
+
+@docs addField, addOptionalField
+
+@docs maybeScalarEncode
+
+@docs encode, toFieldList
+
+-}
+
+import GraphQL.Engine exposing (Option(..))
+import Json.Encode
+import Set
+
+
+{-| We can also accept:
+
+  - Enum values (unquoted)
+  - custom scalars
+
+But we can define anything else in terms of these:
+
+-}
+type Argument obj
+    = ArgValue Json.Encode.Value String
+    | Var String
+
+
+{-| -}
+type InputObject value
+    = InputObject String (List ( String, VariableDetails ))
+
+
+type alias VariableDetails =
+    { gqlTypeName : String
+    , value : Maybe Json.Encode.Value
+    }
+
+
+{-| -}
+inputObject : String -> InputObject value
+inputObject name =
+    InputObject name []
+
+
+{-| -}
+addField : String -> String -> Json.Encode.Value -> InputObject value -> InputObject value
+addField fieldName gqlFieldType val (InputObject name inputFields) =
+    InputObject name
+        (inputFields
+            ++ [ ( fieldName
+                 , { gqlTypeName = gqlFieldType
+                   , value = Just val
+                   }
+                 )
+               ]
+        )
+
+
+{-| -}
+addOptionalField : String -> String -> Option value -> (value -> Json.Encode.Value) -> InputObject input -> InputObject input
+addOptionalField fieldName gqlFieldType optionalValue toJsonValue (InputObject name inputFields) =
+    let
+        newField =
+            case optionalValue of
+                Absent ->
+                    ( fieldName, { value = Nothing, gqlTypeName = gqlFieldType } )
+
+                Null ->
+                    ( fieldName, { value = Just Json.Encode.null, gqlTypeName = gqlFieldType } )
+
+                Present val ->
+                    ( fieldName, { value = Just (toJsonValue val), gqlTypeName = gqlFieldType } )
+    in
+    InputObject name (inputFields ++ [ newField ])
+
+
+{-| -}
+type Optional arg
+    = Optional String (Argument arg)
+
+
+{-| -}
+toFieldList : InputObject a -> List ( String, VariableDetails )
+toFieldList (InputObject _ fields) =
+    fields
+
+
+{-| -}
+encode : InputObject value -> Json.Encode.Value
+encode (InputObject _ fields) =
+    fields
+        |> List.filterMap
+            (\( varName, var ) ->
+                case var.value of
+                    Nothing ->
+                        Nothing
+
+                    Just value ->
+                        Just ( varName, value )
+            )
+        |> Json.Encode.object
+
+
+{-| -}
+encodeOptionals : List (Optional arg) -> List ( String, Argument arg )
+encodeOptionals opts =
+    List.foldl
+        (\(Optional optName argument) (( found, gathered ) as skip) ->
+            if Set.member optName found then
+                skip
+
+            else
+                ( Set.insert optName found
+                , ( optName, argument ) :: gathered
+                )
+        )
+        ( Set.empty, [] )
+        opts
+        |> Tuple.second
+
+
+{-| -}
+maybeScalarEncode : (a -> Json.Encode.Value) -> Maybe a -> Json.Encode.Value
+maybeScalarEncode encoder maybeA =
+    maybeA
+        |> Maybe.map encoder
+        |> Maybe.withDefault Json.Encode.null
