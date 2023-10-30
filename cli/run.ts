4e14f4eaf01c2d57fa7d798ddd44275258afb6fe
Matthew Griffith <mdg.griffith@gmail.com>
2023-10-30 09:06:30 -0400

    bump to 0.11.1

--- cli/run.ts
+++ cli/run.ts
@@ -12,7 +12,7 @@ import decode from "./templates/Decode.elm";
 // @ts-ignore
 globalThis["XMLHttpRequest"] = XMLHttpRequest.XMLHttpRequest;
 
-export const version: string = "0.11.0";
+export const version: string = "0.11.1";
 type Cache = {
   engineVersion: string;
   files: { [namespace: string]: { [name: string]: { modified: Date } } };
